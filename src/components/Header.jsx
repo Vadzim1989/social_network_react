bbf617d763a8f60bbd1697bdfbb3ce297f666166
Vadim Denisovski <vadim.denisovski@gmail.com>
2024-07-26 14:42:27 +0300

    start

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='app-header'>
+            <img src='https://png.pngtree.com/png-clipart/20240613/original/pngtree-lion-head-face-logo-silhouette-black-icon-tattoo-mascot-hand-drawn-png-image_15317165.png' alt='wolf'/>
+        </header>
+    )
+}
+
+export default Header;
